ff562caf1e712f6b3361ad7d1f92e2ec12fc960a
Jorge Bertrán <j.bertran@docline.es>
2020-10-04 11:10:44 +0200

    Install packages for Doctrine

---
+++ two_factor_authenticator/migrations/Version20201004065339.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201004065339 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE verification (id VARCHAR(50) PRIMARY KEY NOT NULL, code VARCHAR(20) NOT NULL, generated_at DATETIME NOT NULL, phone_number VARCHAR(12) NOT NULL)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE verification');
+    }
+}
