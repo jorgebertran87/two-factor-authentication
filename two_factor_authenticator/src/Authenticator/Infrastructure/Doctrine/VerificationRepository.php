b7fca922cbb8b9b970af67d05dd81d453f27d86e
jorgebertran87 <jorgebertran87@gmail.com>
2020-10-10 05:16:35 +0200

    Update VerificationRepository.php
    Remove unused comment

--- two_factor_authenticator/src/Authenticator/Infrastructure/Doctrine/VerificationRepository.php
+++ two_factor_authenticator/src/Authenticator/Infrastructure/Doctrine/VerificationRepository.php
@@ -13,12 +13,7 @@ use DateTimeImmutable;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @method Verification|null find($id, $lockMode = null, $lockVersion = null)
- * @method Verification|null findOneBy(array $criteria, array $orderBy = null)
- * @method Verification[]    findAll()
- * @method Verification[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
+
 class VerificationRepository extends ServiceEntityRepository implements VerificationReadRepository, VerificationWriteRepository
 {
     public function __construct(ManagerRegistry $registry)
